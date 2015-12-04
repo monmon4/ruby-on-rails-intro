68fb6d33e998e9942eb7b9e3e6242c75be78ef2d
Mona Ahmed <mona_ahmed_94@hotmail.com>
2015-12-04 06:43:21 -0800

    moving the code from view into controller action

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+  	random_names = ["alex", "marcos", "lili"]
+  	@name = random_names.sample
+  	@time = Time.now
+  	@time_displayed ||= 0
+  	@time_displayed += 1
   end
   def goodbye
   end
